b93220a767b58ce510340624c05497355b0aa511
uenokenji <salesdesignlabtv@gmail.com>
2016-01-04 09:06:37 +0000

    create message model

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,17 @@
+class MessagesController < ApplicationController
+  def index
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ## ここまで
+end
